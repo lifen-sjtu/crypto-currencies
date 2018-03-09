ca99acf2794520535d8eef9f8435e8077f6f8bcd
Fen Li <lifensjtu@gmail.com>
2018-03-08 22:47:09 -0800

    clean up unnecessary dependencies

--- src/main/java/crypto/repository/ApplicationUserRepository.java
+++
@@ -1,8 +0,0 @@
-package crypto.repository;
-
-import crypto.models.ApplicationUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
-    ApplicationUser findByUsername(String username);
-}
